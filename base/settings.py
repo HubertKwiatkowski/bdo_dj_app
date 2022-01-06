19144eaa8fe8be4bc1a9fa75ee35fa836d4fe482
warlock <h.kwiatkowski@hotmail.com>
2022-01-06 16:48:34 +0100

    added register/login/logout

--- base/settings.py
+++ base/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # my apps
+    'registration.apps.RegistrationConfig',
     'grind.apps.GrindConfig',
     'processing.apps.ProcessingConfig',
     'cooking.apps.CookingConfig',
@@ -130,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- base/urls.py
+++ base/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from registration import views as regV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('registration/', regV.registration, name="registration"),
     path('', include("main.urls")),
     path('cooking/', include("cooking.urls")),
     path('grind/', include("grind.urls")),
